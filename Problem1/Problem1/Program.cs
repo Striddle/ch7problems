a25e66d94e184602247769d027c5ef5cc3c6ee12
Striddle <Striddle@hotmail.com>
2013-09-11 08:43:00 -0400

    more work on problem done

---
+++ Problem1/Problem1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ch7problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizza pizza = new Pizza();
+            pizza.Topping = "pepperoni";
+
+            Pizza pizza = new Pizza();
+            pizza.Diameter = "12";
+
+            Pizza pizza = new Pizza();
+            pizza.Price = "13";
+        }
+    }
+}
---
+++ Problem1/Problem1/TestPizza.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ch7problems
+{
+    class Pizza
+    {
+        // Private DATA variables
+        private string topping;
+        private int diameter;
+        private double price;
+
+        // Public properties that manipulate data variables
+        public string Topping
+        {
+            get
+            {
+                return topping;
+            }
+
+            set
+            {
+                topping = value;
+            }
+        }
+
+        public int Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+
+            set
+            {
+                diameter = 12;
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                return Price;
+            }
+
+            set
+            {
+                Price = 13.99;
+            }
+        }
+    }
+}
+
