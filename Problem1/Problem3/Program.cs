0823cde50d85f898a300c758dbdf85363e27e935
Striddle <Striddle@hotmail.com>
2013-09-13 08:43:44 -0400

    Worked on more projects

---
+++ Problem1/Problem3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ch7problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizza pizza = new Pizza();
+            pizza.Topping = "pepperoni";
+            pizza.Diameter = 12;
+            pizza.Price = 13.99;
+        }
+    }
+}
---
+++ Problem1/Problem3/TestCircle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ch7problems
+{
+    class TestCircle
+    {
+        // Private DATA variables
+        private string radius;
+        private string area;
+        private double diameter;
+
+        // Public properties that manipulate data variables
+        public string Radius
+        {
+            get
+            {
+                return radius;
+            }
+
+            set
+            {
+                radius = value;
+            }
+        }
+
+        public double Diameter
+        {
+            get
+            {
+                return radius * 2;
+            }
+
+        }
+        public string Area
+        {
+            get
+            {
+                return area;
+            }
+
+            set
+            {
+                area = value;
+            }
+        }
+    }
+}
